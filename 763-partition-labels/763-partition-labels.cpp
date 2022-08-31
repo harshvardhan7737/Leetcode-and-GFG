805846f5c317243bd19723ddfaa1587c2106e949
Harshvardhan Singh <84776690+cyberskull99297@users.noreply.github.com>
2022-08-31 15:00:15 +0530

    Time: 10 ms (29.47%), Space: 6.8 MB (23.85%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int l=0;
-        int h = s.size()-1;
-        vector<vector<int>> ans;
-        set<char> ourset;
+        
+        unordered_map<char,int> m;
         
         for(int i=0; i<s.size(); i++)
         {
-            h = s.size()-1;
-            
-            while(h>=i) 
-            {
-                if(s[i]==s[h] && ourset.find(s[i])==ourset.end())
-                {
-                    ourset.insert(s[i]);
-                    ans.push_back({i,h});
-                    break;
-                }
-                
-                h--;         
-            }       
+            m[s[i]] = i;
         }
         
-        vector<vector<int>> res;
-        vector<int> temp;
-        res.push_back(ans[0]);
+        vector<int> ans;
+        int maximum=0;
+        int prev = -1;
         
-        for(int i=1; i<ans.size(); i++)
+        for(int i=0; i<s.size(); i++)
         {
-            if(res[res.size()-1][1] > ans[i][0])
-            {
-                res[res.size()-1][1] = max(res[res.size()-1][1],ans[i][1]);
-            }
+           maximum = max(maximum,m[s[i]]);
             
-            else
+            if(maximum == i)
             {
-                res.push_back(ans[i]);
+                ans.push_back(i - prev);
+                prev = i;
             }
         }
-        
-        for(int i=0; i<res.size(); i++)
-        {
-         temp.push_back(res[i][1] - res[i][0] + 1);
-        }
-        
-        return temp;
+        return ans;
     }
-    
 };
